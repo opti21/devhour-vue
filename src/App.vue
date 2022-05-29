68b0eeec2bd1520dce0a3e843be9b805913b9d0f
opti21 <40129778+opti21@users.noreply.github.com>
2022-05-28 23:22:38 -0500

    init

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import YoutubeVideo from "./components/YoutubeVideo.vue";
+import splitbee from "@splitbee/web";
+
+splitbee.init();
+</script>
+
+<template>
+  <div>
+    <h1>DEVHOUR PODCAST</h1>
+    <YoutubeVideo ytURL="https://www.youtube.com/embed/6Ab6ka_MKJ4" />
+    <YoutubeVideo ytURL="https://www.youtube.com/embed/pvBHyip4peo" />
+  </div>
+</template>
---
+++ src/components/YoutubeVideo.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+defineProps<{
+  ytURL: string;
+}>();
+</script>
+
+<template>
+  <iframe
+    width="560"
+    height="315"
+    :src="ytURL"
+    title="YouTube video player"
+    frameborder="0"
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    allowfullscreen
+  ></iframe>
+</template>
